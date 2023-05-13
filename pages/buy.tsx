54d5b69e718a8c5b301a7b35e4230b0f41237ef6
Jeff3rs0n <128228045+Jeff3rs0n@users.noreply.github.com>
2023-05-13 20:50:46 +0300

    rare

--- pages/buy.tsx
+++ pages/buy.tsx
@@ -1,55 +1,25 @@
-import type { NFT as NFTType } from "@thirdweb-dev/sdk";
-import Link from "next/link";
+import { useContract, useNFTs } from "@thirdweb-dev/react";
 import React from "react";
+import Container from "../components/Container/Container";
+import NFTGrid from "./NFTGrid";
 import { NFT_COLLECTION_ADDRESS } from "../const/contractAddresses";
-import Skeleton from "./Skeleton/Skeleton";
-import NFT from "../components/NFT";
-import styles from "../styles/Buy.module.css";
 
-type Props = {
-  isLoading: boolean;
-  data: NFTType[] | undefined;
-  overrideOnclickBehavior?: (nft: NFTType) => void;
-  emptyText?: string;
-};
+export default function Buy() {
+  // Load all of the NFTs from the NFT Collection
+  const { contract } = useContract(NFT_COLLECTION_ADDRESS);
+  const { data, isLoading } = useNFTs(contract);
 
-export default function NFTGrid({
-  isLoading,
-  data,
-  overrideOnclickBehavior,
-  emptyText = "No NFTs found for this collection.",
-}: Props) {
   return (
-    <div className={styles.nftGridContainer}>
-      {isLoading ? (
-        [...Array(20)].map((_, index) => (
-          <div key={index} className={styles.nftContainer}>
-            <Skeleton key={index} width={"100%"} height="312px" />
-          </div>
-        ))
-      ) : data && data.length > 0 ? (
-        data.map((nft) =>
-          !overrideOnclickBehavior ? (
-            <Link
-              href={`/token/${NFT_COLLECTION_ADDRESS}/${nft.metadata.id}`}
-              key={nft.metadata.id}
-              className={styles.nftContainer}
-            >
-              <NFT nft={nft} />
-            </Link>
-          ) : (
-            <div
-              key={nft.metadata.id}
-              className={styles.nftContainer}
-              onClick={() => overrideOnclickBehavior(nft)}
-            >
-              <NFT nft={nft} />
-            </div>
-          )
-        )
-      ) : (
-        <p>{emptyText}</p>
-      )}
-    </div>
+    <Container maxWidth="lg">
+      <h1>Buy NFTs</h1>
+      <p>Browse which NFTs are available from the collection.</p>
+      <NFTGrid
+        data={data}
+        isLoading={isLoading}
+        emptyText={
+          "Looks like there are no NFTs in this collection. Did you import your contract on the thirdweb dashboard? https://thirdweb.com/dashboard"
+        }
+      />
+    </Container>
   );
 }
