95feab5b6a627ea87cbc7a64604cb1fa4fc6f136
Jeff3rs0n <128228045+Jeff3rs0n@users.noreply.github.com>
2023-05-15 11:47:59 +0300

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,20 +1,25 @@
-import { Navbar, Link, Text, Avatar, Dropdown, Container, Image} from "@nextui-org/react";
+import {
+  Navbar,
+  Link,
+  Text,
+  Avatar,
+  Dropdown,
+  Container,
+  Image,
+} from "@nextui-org/react";
 import { Layout } from "./Layout.js";
-import { useTheme as useNextTheme } from 'next-themes'
-import { Switch, useTheme, Spacer } from '@nextui-org/react'
+import { useTheme as useNextTheme } from "next-themes";
+import { Switch, useTheme, Spacer } from "@nextui-org/react";
 
 import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
 import styles from "./Navbar.module.css";
-
 
 export default function Header() {
   const { setTheme } = useNextTheme();
   const { isDark, type } = useTheme();
 
- 
-
   const address = useAddress();
-   const collapseItems = [
+  const collapseItems = [
     "Profile",
     "Dashboard",
     "Activity",
@@ -28,11 +33,13 @@ export default function Header() {
   ];
 
   return (
-    <Container       css={{
-      maxW: "100%",
-      minWidth: "100%"
-    }}>
- <Navbar isBordered variant="static" >
+    <Container
+      css={{
+        maxW: "100%",
+        minWidth: "100%",
+      }}
+    >
+      <Navbar isBordered variant="static">
         <Navbar.Toggle showIn="xs" />
         <Navbar.Brand
           css={{
@@ -41,56 +48,65 @@ export default function Header() {
             },
           }}
         >
- <Link href="/">
- <Text b color="warning" 
-          css={{
-            fontFamily: "monospace",
-            textGradient: "45deg, $yellow600 -20%, $red600 100%",
-          }}
-          >
-           RareBay 
-          </Text>
-         
-          <Image
-          src="https://bafkreiakjtw7wj6x7qpdliuoeypuar2554hebk3vnz75dhomkqrgbwvfya.ipfs.nftstorage.link/"
-          alt="logo"
-          width={50}
-          height={50}
-        />
- </Link>
-         
+          <Link href="/">
+            <Text
+              b
+              color="warning"
+              css={{
+                fontFamily: "monospace",
+                textGradient: "45deg, $yellow600 -20%, $red600 100%",
+              }}
+            >
+              RareBay
+            </Text>
+
+            <Image
+              src="https://bafkreiakjtw7wj6x7qpdliuoeypuar2554hebk3vnz75dhomkqrgbwvfya.ipfs.nftstorage.link/"
+              alt="logo"
+              width={50}
+              height={50}
+            />
+          </Link>
         </Navbar.Brand>
-        <Navbar.Content
-          activeColor="warning"
-          hideIn="xs"
-          variant="light"
-        >
-          <Navbar.Link href="/" isActive> <Text b color="warning" 
-          css={{
-            fontFamily: "monospace"
-          }}
-          >
-           Marketplace
-          </Text></Navbar.Link>
-          <Navbar.Link  href="#">
-          <Text b color="warning" 
-          css={{
-            fontFamily: "monospace"
-          }}
-          >
-           Categories
-          </Text>
+        <Navbar.Content activeColor="warning" hideIn="xs" variant="light">
+          <Navbar.Link href="/" isActive>
+            {" "}
+            <Text
+              b
+              color="warning"
+              css={{
+                fontFamily: "monospace",
+              }}
+            >
+              Marketplace
+            </Text>
           </Navbar.Link>
-          <Navbar.Link href="#"> <Text b color="warning" 
-          css={{
-            fontFamily: "monospace"
-          }}
-          >
-           Airdrop 
-          </Text></Navbar.Link>
+          <Navbar.Link href="#">
+            <Text
+              b
+              color="warning"
+              css={{
+                fontFamily: "monospace",
+              }}
+            >
+              Categories
+            </Text>
+          </Navbar.Link>
+          <Navbar.Link href="#">
+            {" "}
+            <Text
+              b
+              color="warning"
+              css={{
+                fontFamily: "monospace",
+              }}
+            >
+              Airdrop
+            </Text>
+          </Navbar.Link>
           <Spacer />
         </Navbar.Content>
-        
+
         <Navbar.Content
           css={{
             "@xs": {
@@ -98,15 +114,11 @@ export default function Header() {
               jc: "flex-end",
             },
           }}
-        >  
-
-<ConnectWallet btnTitle="Sign In"  />
+        >
           <Dropdown placement="bottom-right">
-
             <Navbar.Item>
               <Dropdown.Trigger>
-              
-           <Avatar
+                <Avatar
                   bordered
                   as="button"
                   color="warning"
@@ -122,34 +134,67 @@ export default function Header() {
               onAction={(actionKey) => console.log({ actionKey })}
             >
               <Dropdown.Item key="profile" css={{ height: "$18" }}>
-              <div className={styles.navRight}>
-              <Link href="/buy"><Text color="warning" css={{ fontFamily: "monospace"}}>Wallet</Text></Link>
-          {address && (
-            <Link className={styles.link} href={`/profile/${address}`}>
-
-            </Link>
-          )}
-        </div>
-
+                <div className={styles.navRight}>
+                <ConnectWallet btnTitle="Sign In"  />
+                  {address && (
+                    <Link
+                      className={styles.link}
+                      href={`/profile/${address}`}
+                    ></Link>
+                  )}
+                </div>
               </Dropdown.Item>
-              <Dropdown.Item key="buy" withDivider><Link href="/buy"><Text color="warning" css={{ fontFamily: "monospace"}}>Buy NFT</Text></Link>
+              <Dropdown.Item key="buy" withDivider>
+                <Link href="/buy">
+                  <Text color="warning" css={{ fontFamily: "monospace" }}>
+                    Buy NFT
+                  </Text>
+                </Link>
               </Dropdown.Item>
-              <Dropdown.Item key="sell"><Link href="/sell"> <Text color="warning" css={{ fontFamily: "monospace"}}>Sell NFT</Text></Link>
+              <Dropdown.Item key="sell">
+                <Link href="/sell">
+                  {" "}
+                  <Text color="warning" css={{ fontFamily: "monospace" }}>
+                    Sell NFT
+                  </Text>
+                </Link>
               </Dropdown.Item>
-              <Dropdown.Item key="create"><Text color="warning" css={{ fontFamily: "monospace"}}>Create NFT</Text></Dropdown.Item>
-              <Dropdown.Item key="collections" withDivider><Link href={`/profile/${address}`}><Text color="warning" css={{ fontFamily: "monospace"}}>My Collections</Text></Link>
+              <Dropdown.Item key="create">
+                <Text color="warning" css={{ fontFamily: "monospace" }}>
+                  Create NFT
+                </Text>
               </Dropdown.Item>
-              <Dropdown.Item key="portfolio"><Link href={`/profile/${address}`}><Text color="warning" css={{ fontFamily: "monospace"}}>My Portfolio</Text></Link></Dropdown.Item>
-              <Dropdown.Item key="configurations"><Link href="/create"><Text color="warning" css={{ fontFamily: "monospace"}}>Create Launchad</Text></Link></Dropdown.Item>
-              <Dropdown.Item key="theme" >
-              <Switch
-        checked={isDark}
-        onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
-      />
+              <Dropdown.Item key="collections" withDivider>
+                <Link href={`/profile/${address}`}>
+                  <Text color="warning" css={{ fontFamily: "monospace" }}>
+                    My Collections
+                  </Text>
+                </Link>
+              </Dropdown.Item>
+              <Dropdown.Item key="portfolio">
+                <Link href={`/profile/${address}`}>
+                  <Text color="warning" css={{ fontFamily: "monospace" }}>
+                    My Portfolio
+                  </Text>
+                </Link>
+              </Dropdown.Item>
+              <Dropdown.Item key="configurations">
+                <Link href="/create">
+                  <Text color="warning" css={{ fontFamily: "monospace" }}>
+                    Create Launchad
+                  </Text>
+                </Link>
+              </Dropdown.Item>
+              <Dropdown.Item key="theme">
+                <Switch
+                  checked={isDark}
+                  onChange={(e) =>
+                    setTheme(e.target.checked ? "dark" : "light")
+                  }
+                />
               </Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
-
         </Navbar.Content>
         <Navbar.Collapse>
           {collapseItems.map((item, index) => (
@@ -174,7 +219,6 @@ export default function Header() {
           ))}
         </Navbar.Collapse>
       </Navbar>
-    
     </Container>
   );
 }
